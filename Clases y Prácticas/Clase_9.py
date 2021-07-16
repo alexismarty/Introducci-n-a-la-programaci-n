b571d3ddd6b09c21fc6942186d13142f235f58cf
alexismarty <87547976+alexismarty@users.noreply.github.com>
2021-07-16 19:30:07 -0300

    Create Clase_9.py

---
+++ Clases y Prácticas/Clase_9.py
@@ -0,0 +1,20 @@
+"EJERCICIO: CONTROL DE CLAVE"
+
+MAX_INTENTOS = 3
+
+clInterna = "123Abcde"
+usuarioValidado = False
+
+for intentos in range(MAX_INTENTOS):
+  clave = input("Ingresar clave ")
+  if(clave == clInterna):
+    usuarioValidado = True
+    print ("Clave correcta")
+    break
+  else:
+    print ("Clave incorrecta")
+
+if(usuarioValidado == True):
+    print("SISTEMA OK")
+else:
+    print("SISTEMA BLOQUEADO")
